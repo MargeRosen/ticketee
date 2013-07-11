78d63f9bbac64231476e399c1232d0a4ed3fc1f7
Marge Rosen <marge@masterclassu.com>
2013-07-11 14:13:35 -0700

    Add title functionality for show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,22 +2,24 @@ class ProjectsController < ApplicationController
 	def index
 	end	
 
-	def new
-		 @project = Project.new
+	def new  #blank form
+		logger.info "message for log file:  processed new method in ProjectsController"
+		@project = Project.new
 	end
 
-	def create
+	def create  #based on user input
   	@project = Project.new(params[:project]) 
-  	if @project.save
+  	if @project.save  # returns true or false
     	flash[:notice] = "Project has been created."
     	redirect_to @project
   	else
-    # nothing, yet
+   		flash[:notice] = "Project has NOT been created."
+   		render :action => "new" 
   	end
 	end
 
 	def show
-  @project = Project.find(params[:id])
+  	@project = Project.find(params[:id]) #find method on class Project using ActiveRecord
 	end
 
 end
