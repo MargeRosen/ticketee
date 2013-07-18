bae770b72d344af336cf768489262a6d39fec2f4
Marge Rosen <marge@masterclassu.com>
2013-07-18 14:01:16 -0700

    Add the ability to create users through the admin backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Ticketee::Application.routes.draw do
-  get "/admin/users/index"
-
+#  get "/admin/users/index" #only responds to URL + HTTP method
   devise_for :users
+
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id))(.:format)'
+  root :to => "projects#index"
+  resources :projects do
+    resources :tickets  # nested routing helper
+  end
+
+  namespace :admin do
+    root :to => "base#index"
+    resources :users
+  end
+end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,11 +69,3 @@ Ticketee::Application.routes.draw do
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
-  root :to => "projects#index"
-  #resources :projects
-  resources :projects do
-  resources :tickets  # nested routing helper
-end
-end
