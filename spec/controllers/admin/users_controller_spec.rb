655844821fc8c18026293ea2d55256b6706d5777
Marge Rosen <marge@masterclassu.com>
2013-07-18 15:42:49 -0700

    Add ability to edit and update users

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -3,15 +3,6 @@ require 'spec_helper'
 describe Admin::UsersController do
   let(:user) { Factory(:confirmed_user) }
 
-#  Anon users and standard users should redirect?
-#  context "anonymous users" do
-#    it "are not able to access the index action" do
-#      get 'index'
-#      response.should redirect_to('/')
-#      flash[:alert].should eql("You must be an admin to do that.")
-#    end
-#  end
-
   context "standard users" do
     before { sign_in(:user, user) }
 
